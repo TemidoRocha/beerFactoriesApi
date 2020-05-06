5d94abe4bcc2e20aafcd3e63a8fc15b8817a94b9
TemidoRocha <manueltemidorocha@gmail.com>
2020-05-06 16:04:57 +0100

    server started

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
 const serveFavicon = require('serve-favicon');
-const indexRouter = require('./beerfactoriesapi/routes/index');
+const indexRouter = require('./routes/index');
 
 const app = express();
 
